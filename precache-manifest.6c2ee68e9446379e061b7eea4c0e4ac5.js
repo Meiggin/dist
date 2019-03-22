57d85aafcfd69ca3bb9084452405a376a8634dff
Meiggin <781571871@qq.com>
2019-03-22 17:14:12 +0800

    11

---
+++ precache-manifest.6c2ee68e9446379e061b7eea4c0e4ac5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "17aa99f304301956d64a1e473da7ac8b",
+    "url": "index.html"
+  },
+  {
+    "revision": "177545d127de7373a094b96bb99f01a8",
+    "url": "img/timg.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "41d6608af83635e21e46",
+    "url": "js/app.3f82e9bf.js"
+  },
+  {
+    "revision": "05640542c03b5583e39e360f70a6cd5e",
+    "url": "img/timg2.jpg"
+  },
+  {
+    "revision": "3e9c7dd00de27969b248",
+    "url": "js/chunk-vendors.bed4cb75.js"
+  },
+  {
+    "revision": "0779e963b6b232d1efc1",
+    "url": "js/about.52fe6c20.js"
+  },
+  {
+    "revision": "05640542c03b5583e39e360f70a6cd5e",
+    "url": "img/timg2.05640542.jpg"
+  },
+  {
+    "revision": "fecd67c3bed879bfe5342609f5b590ba",
+    "url": "img/jt2.png"
+  },
+  {
+    "revision": "b2d0712fda0964d2b678a22ad3a1f872",
+    "url": "img/jt1.png"
+  },
+  {
+    "revision": "0ec491597901061ebbfa0fc6eb691f43",
+    "url": "img/BG2.png"
+  },
+  {
+    "revision": "8d5b592a92df22f7e805ee4736a68bce",
+    "url": "img/BG.png"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "5b7c650554aaba9c298eddcb36fbabdd",
+    "url": "data.json"
+  },
+  {
+    "revision": "3e9c7dd00de27969b248",
+    "url": "css/chunk-vendors.983ca916.css"
+  },
+  {
+    "revision": "41d6608af83635e21e46",
+    "url": "css/app.a7c3786f.css"
+  },
+  {
+    "revision": "0779e963b6b232d1efc1",
+    "url": "css/about.eb4225e6.css"
+  }
+];
